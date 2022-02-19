e7fcf00818fd33eb5c6b3869e5105529047dd462
avazbekdeveloper11@gmail.com <you@example.com>
2022-02-19 09:12:08 +0500

    router yozildi

---
+++ lib/router/router.dart
@@ -0,0 +1,13 @@
+import 'package:fast_food/screens/first_page.dart';
+import 'package:flutter/material.dart';
+
+class GenerateRoute {
+  Route? generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (context) => const FirstPage());
+    }
+  }
+}
